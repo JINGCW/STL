e9401ec03c9b6bdaaf9d3d251f80749c46792a1a
JINGCW <1562359870@qq.com>
2020-06-19 23:09:18 +0800

    multidir/multisrc

---
+++ CMakeFiles/stl_exx.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stl_exx.dir/main.cc.obj"
+  "stl_exx.exe"
+  "stl_exx.exe.manifest"
+  "stl_exx.lib"
+  "stl_exx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stl_exx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ stl/CMakeFiles/_primer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_primer.dir/src/_primer/_6_function.cc.obj"
+  "CMakeFiles/_primer.dir/src/_primer/_7_class.cc.obj"
+  "_primer.lib"
+  "_primer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_primer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
