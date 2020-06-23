8ff68c980635b77f8c60af09774f64eda5d78a7d
JINGCW <1562359870@qq.com>
2020-06-24 00:20:28 +0800

    9-string search operator

--- main.cc
+++ main.cc
@@ -1,9 +1,11 @@
 #include "stl/src/_primer/_7_class.h"
 #include "stl/src/_primer/_6_function.h"
 #include "stl/src/_primer/_8_IO.h"
+#include "stl/src/_primer/_9_sequential_container.h"
 
 int main(int argc, char *argv[]){
 //    getter_test();
-    save_person_info();
+    change_container();
+//    save_person_info();
     return 0;
 }
--- stl/src/_primer/_9_sequential_container.cpp
+++ stl/src/_primer/_9_sequential_container.cpp
@@ -4,14 +4,16 @@
 #include <string>
 #include <vector>
 #include <forward_list>
+#include <iostream>
 #include "_9_sequential_container.h"
 
 using namespace std;
 
-void container_init_stub()
- {
-    list<string >authors={"milton","shakespeare","austen"};
-    vector<const char*>articles={"a","an","the"};
+void container_init_stub() {
+    list<string> authors = {"milton", "shakespeare", "austen"};
+    cout << "authors: " << authors.size() << " "
+         << authors.max_size() << endl;
+    vector<const char *> articles = {"a", "an", "the"};
 
     /*success:type match*/
 //     list<string> list2(authors);
@@ -22,4 +24,28 @@ void container_init_stub()
 //    vector<string> words(articles);
     /*success:convert Elem char* to string*/
     forward_list<string> words(articles.begin(), articles.end());
+}
+
+void recursive_insert() {
+    string word;
+    list<string> lst;
+    auto iter = lst.begin();
+
+    while (cin >> word)
+        iter = lst.insert(iter, word);
+}
+
+void change_container() {
+    vector<int> iv = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    auto iter = iv.begin();
+    while (iter != iv.end()) {
+        if (*iter % 2) {
+            iter = iv.insert(iter, *iter);
+            iter += 2;
+        } else
+            iter = iv.erase(iter);
+    }
+    for (const auto &e:iv) {
+        cout << e << endl;
+    }
 }
