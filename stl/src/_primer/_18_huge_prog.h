72fc5ba7375d4ff847a21d0c7fdc9d72e1ca832c
JINGCW <1562359870@qq.com>
2020-07-18 23:33:34 +0800

    namespace

--- stl/src/_primer/_18_huge_prog.h
+++ stl/src/_primer/_18_huge_prog.h
@@ -12,13 +12,59 @@
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 class _Exception
 {
 public:
     _Exception(initializer_list<T>);
+
 private:
     shared_ptr<vector<T>> data;
 };
+
+namespace hug_prog
+{
+    class Base
+    {
+    public:
+        virtual double f1(double) noexcept;
+
+        virtual int f2() noexcept(false);
+
+        virtual void f3();
+    };
+
+    class Derived : public Base
+    {
+        virtual double f1(double) noexcept override;
+
+        virtual int f2() noexcept(false);
+
+        virtual void f3() noexcept;
+    };
+
+    class out_of_stock : public runtime_error
+    {
+    public:
+        explicit out_of_stock(const string &s) :
+                runtime_error(s)
+        {}
+    };
+
+    class isbn_mismatch : public logic_error
+    {
+    public:
+        explicit isbn_mismatch(const string &s) :
+                logic_error(s)
+        {}
+
+        isbn_mismatch(const string &s, const string &lhs, const string &rhs) :
+                logic_error(s), left(lhs), right(rhs)
+        {}
+
+        const string left, right;
+    };
+}
+
 
 #endif //STANDARD_TEMPLATE_LIBRARIES__18_HUGE_PROG_H
