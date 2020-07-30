b7343b15d7ce93e8d4652bb1cf7f4dc763f016c5
JINGCW <1562359870@qq.com>
2020-07-30 23:55:34 +0800

    cmake configure_file install

---
+++ config.h
@@ -0,0 +1,6 @@
+//#define USE_Tetris
+#define USE_THIS_Boost
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 2
--- main.cc
+++ main.cc
@@ -4,28 +4,33 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 /* Shows how to use both command line and config file. */
+#include "config.h"
+#ifdef USE_THIS_Boost
 
 #include <boost/program_options.hpp>
+
 namespace po = boost::program_options;
 
 
 #include <iostream>
 #include <fstream>
 #include <iterator>
+
 using namespace std;
 
 // A helper function to simplify the main part.
 template<class T>
-ostream& operator<<(ostream& os, const vector<T>& v)
+ostream &operator<<(ostream &os, const vector <T> &v)
 {
     copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
     return os;
 }
 
 
-int main(int ac, char* av[])
+int main(int ac, char *av[])
 {
-    try {
+    try
+    {
         int opt;
         string config_file;
 
@@ -36,27 +41,24 @@ int main(int ac, char* av[])
                 ("version,v", "print version string")
                 ("help", "produce help message")
                 ("config,c", po::value<string>(&config_file)->default_value("multiple_sources.cfg"),
-                 "name of a file of a configuration.")
-                ;
+                 "name of a file of a configuration.");
 
         // Declare a group of options that will be
         // allowed both on command line and in
         // config file
         po::options_description config("Configuration");
         config.add_options()
-                ("optimization", po::value<int>(&opt)->default_value(10),
+                ("optimization,O", po::value<int>(&opt)->default_value(10),
                  "optimization level")
                 ("include-path,I",
-                 po::value< vector<string> >()->composing(),
-                 "include path")
-                ;
+                 po::value < vector < string > > ()->composing(),
+                 "include path");
 
         // Hidden options, will be allowed both on command line and
         // in config file, but will not be shown to the user.
         po::options_description hidden("Hidden options");
         hidden.add_options()
-                ("input-file", po::value< vector<string> >(), "input file")
-                ;
+                ("input-file", po::value < vector < string > > (), "input file");
 
 
         po::options_description cmdline_options;
@@ -81,19 +83,20 @@ int main(int ac, char* av[])
         {
             cout << "can not open config file: " << config_file << "\n";
             return 0;
-        }
-        else
+        } else
         {
             store(parse_config_file(ifs, config_file_options), vm);
             notify(vm);
         }
 
-        if (vm.count("help")) {
+        if (vm.count("help"))
+        {
             cout << visible << "\n";
             return 0;
         }
 
-        if (vm.count("version")) {
+        if (vm.count("version"))
+        {
             cout << "Multiple sources example, version 1.0\n";
             return 0;
         }
@@ -101,21 +104,23 @@ int main(int ac, char* av[])
         if (vm.count("include-path"))
         {
             cout << "Include paths are: "
-                 << vm["include-path"].as< vector<string> >() << "\n";
+                 << vm["include-path"].as < vector < string > > () << "\n";
         }
 
         if (vm.count("input-file"))
         {
             cout << "Input files are: "
-                 << vm["input-file"].as< vector<string> >() << "\n";
+                 << vm["input-file"].as < vector < string > > () << "\n";
         }
 
         cout << "Optimization level is " << opt << "\n";
     }
-    catch(exception& e)
+    catch (exception &e)
     {
         cout << e.what() << "\n";
         return 1;
     }
     return 0;
 }
+
+#endif
