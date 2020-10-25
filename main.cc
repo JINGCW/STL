c6dc887f1f65c7e28967bad1da18461364d215a3
qiming.xie <1562359870@qq.com>
2020-10-25 23:28:53 +0800

    ::

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 // Copyright Vladimir Prus 2002-2004.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt
+// (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 /* Shows how to use both command line and config file. */
